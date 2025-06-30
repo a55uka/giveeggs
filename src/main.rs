52ac4e099565bb3cbee6e78ffdb946ee349f5853
ElonGaties <59313695+ElonGaties@users.noreply.github.com>
2025-07-01 00:00:23 +0100

    feat: move into thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::error::Error;
 use std::time::Duration;
-use tracing::info;
+use tokio::signal;
+use tracing::{error, info};
+
 use giveeggs::Eggs;
 
 const WAIT: Duration = Duration::from_secs(50);
+const FAIL_WAIT: Duration = Duration::from_secs(20);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -11,13 +14,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Starting checker...");
 
-    let mut eggs = Eggs::new("https://eggdot.net/", ("https://ntfy.sh", "shopify_eggdot-net"), vec![8314152616094]).await?;
+    let worker = tokio::spawn(async {
+        let mut eggs =
+            match Eggs::new("https://eggdot.net/", ("https://ntfy.sh", "shopify_eggdot-net"), vec![8314152616094]).await {
+                Ok(eggs) => eggs,
+                Err(e) => {
+                    error!("Failed to create Eggs instance: {}", e);
+                    return 1;
+                },
+            };
 
-    loop {
-        info!("Checking for new giveeggs...");
-        eggs.notify_changes().await?;
-        info!("Waiting {} seconds...", WAIT.as_secs());
-        tokio::time::sleep(WAIT).await;
+        loop {
+            info!("Checking for new giveeggs...");
+            let ntfy = eggs.notify_changes().await.unwrap();
+            info!("Waiting {} seconds...", WAIT.as_secs());
+            tokio::time::sleep(WAIT).await;
+        }
+    });
+
+    tokio::select! {
+        _ = worker => {},
+        _ = signal::ctrl_c() => {
+            info!("Shutdown signal received. Cleaning up...");
+        }
     }
+
+    info!("Giveeggs stopped gracefully.");
+
     Ok(())
 }
